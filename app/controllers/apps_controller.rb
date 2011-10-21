ffbbb00143ece4ff20b730f1dccea4128749c13c
Bradley Spaulding <bspaulding@dataxu.com>
2011-10-21 00:11:00 -0400

    Initial Commit.

---
+++ app/controllers/apps_controller.rb
@@ -0,0 +1,5 @@
+class AppsController < ApplicationController
+  def index
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhonegapOta::Application.config.secret_token = 'a11d52cbace6f3d7216344f3b50e05c5a10c718553067fa76084b476efd4ed2cb4d66efd72713b7f9a993b288ba3b36dd6ceeeabfabe1fc6157d45c48eba6b0e'
